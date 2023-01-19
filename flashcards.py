c8828fbc0ad27eecbcac655a9165de178fe87070
syth <cjdeni205@gmail.com>
2023-01-19 12:49:36 -0500

    Added colors that correspond to answer. Set definitive window size

--- flashcards.py
+++ flashcards.py
@@ -29,33 +29,41 @@ CHARACTER_DICT = {
 
 class App:
     def __init__(self, window):
+        window.geometry("900x650")
         window.title("Flashcards")
-        window.rowconfigure([0,2], minsize=175, weight=1)
-        window.columnconfigure([0, 4], minsize=100, weight=1)
+        window.rowconfigure([0,2], minsize=250, weight=1)
+        window.columnconfigure([0, 3], minsize=250, weight=1)
 
-        character_label = tk.Label(window, text="", font=("Serif Sans", 90))
-        character_label.grid(row=0, column=1)
+        back = tk.Frame(master=window, bg="black")
+        back.grid_propagate(0)
 
-        correct_label = tk.Label(window, text="Correct: 0", font=("Serif Sans", 30))
-        correct_label.grid(row=0, column=3)
 
-        incorrect_label = tk.Label(window, text="Incorrect: 0", font=("Serif Sans", 30))
-        incorrect_label.grid(row=1, column=3)
+        character_label = tk.Label(window, text="", font=("Serif Sans", 130))
+        character_label.grid(row=0, column=1, sticky="nsew")
 
-        ratio_label = tk.Label(window, text="% Accurate", font=("Sans Serif", 30))
-        ratio_label.grid(row=2, column=3)
+        correct_label = tk.Label(window, text="Correct Guesses: 0", font=("Serif Sans", 30), fg="green", wraplength=130, justify="center")
+        correct_label.grid(row=0, column=3, sticky="nsew")
 
-        answer_label = tk.Label(window, text="", font=("Serif Sans", 25))
-        answer_label.grid(row=1, column=0)
+        incorrect_label = tk.Label(window, text="Incorrect Guesses: 0", font=("Serif Sans", 30), fg="red", wraplength=130, justify="center")
+        incorrect_label.grid(row=1, column=3, sticky="nsew")
 
-        user_input = tk.Entry(window, font=("Serif Sans", 16), )
-        user_input.grid(row=2, column=1)
+        ratio_label = tk.Label(window, text="0.0% Accuracy", font=("Sans Serif", 25))
+        ratio_label.grid(row=2, column=3, sticky="nsew")
+
+        guess_label = tk.Label(window, text="Previous Answer Result:", font=("Serif Sans", 30), wraplength=125, justify="center")
+        guess_label.grid(row=0, column=0, sticky="nsew")
+
+        answer_label = tk.Label(window, text="", font=("Serif Sans", 30))
+        answer_label.grid(row=1, column=0, sticky="nsew")
+
+        user_input = tk.Entry(window, font=("Serif Sans", 16), width=25)
+        user_input.grid(row=2, column=1, padx=20)
 
         answer_button = tk.Button(master=window, text="Answer", command=lambda: answer(character_label, answer_label, user_input, correct_label, incorrect_label, ratio_label))
-        answer_button.grid(row=4, column=1)
+        answer_button.grid(row=4, column=1, sticky="ew")
 
         next_button = tk.Button(master=window, text="Skip Word", command=lambda: next_word(character_label, answer_label, user_input))
-        next_button.grid(row=4, column=3)
+        next_button.grid(row=4, column=2, sticky="ew")
 
         next_word(character_label, answer_label, user_input) # runs the next_word function when the program starts
 
@@ -67,7 +75,7 @@ def next_word(character_label, answer_label, user_input):
     """
     Gets the next character(s) at random
     """
-    answer_label.config(text="")
+
     user_input.delete(0, 'end')
     rand_word = random.choice(list(CHARACTER_DICT.keys()))
     character_label.config(text=rand_word)
@@ -87,19 +95,21 @@ def answer(character_label, answer_label, user_input, correct_label, incorrect_l
     total = TOTAL_POINTS
 
     if user_input.get() == value:
-        answer_label.config(text="Correct Answer!")
+        answer_label.config(text="Correct Answer!", fg="green")
         correct_score = correct_label['text']
         correct = int(''.join(filter(str.isdigit, correct_score)))
         correct +=1
-        correct_label.config(text="Correct: " + str(correct))
+        correct_label.config(text="Correct Guesses: " + str(correct))
         total += 1
+        next_word(character_label, answer_label, user_input)
     else:
-        answer_label.config(text="Incorrect Answer.")
+        answer_label.config(text="Incorrect Answer.", fg="red")
         incorrect_score = incorrect_label['text']
         incorrect = int(''.join(filter(str.isdigit, incorrect_score)))
         incorrect += 1
-        incorrect_label.config(text="Incorrect: "+ str(incorrect))
+        incorrect_label.config(text="Incorrect Guesses: "+ str(incorrect))
         total += 1
+        next_word(character_label, answer_label, user_input)
 
     CORRECT_POINTS = correct
     INCORRECT_POINTS = incorrect
