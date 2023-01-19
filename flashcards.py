fb23c70f899e2d0c63ad1d861c0e6e3be25d087c
syth <cjdeni205@gmail.com>
2023-01-19 10:18:18 -0500

    Added accuracy meter & Adjusted spacings

--- flashcards.py
+++ flashcards.py
@@ -5,8 +5,9 @@ A simple program made to help me learn Japanese characters
 import tkinter as tk
 import random
 
-global CORRECT_POINTS
-global INCORRECT_POINTS
+CORRECT_POINTS = 0
+INCORRECT_POINTS = 0
+TOTAL_POINTS = 0
 
 CHARACTER_DICT = {
     "あ":"a",
@@ -33,27 +34,33 @@ class App:
         window.columnconfigure([0, 4], minsize=100, weight=1)
 
         character_label = tk.Label(window, text="", font=("Serif Sans", 90))
-        character_label.grid(row=0, column=0)
+        character_label.grid(row=0, column=1)
 
         correct_label = tk.Label(window, text="Correct: 0", font=("Serif Sans", 30))
-        correct_label.grid(row=0, column=1)
+        correct_label.grid(row=0, column=3)
 
         incorrect_label = tk.Label(window, text="Incorrect: 0", font=("Serif Sans", 30))
-        incorrect_label.grid(row=1, column=1)
+        incorrect_label.grid(row=1, column=3)
+
+        ratio_label = tk.Label(window, text="% Accurate", font=("Sans Serif", 30))
+        ratio_label.grid(row=2, column=3)
 
         answer_label = tk.Label(window, text="", font=("Serif Sans", 25))
         answer_label.grid(row=1, column=0)
 
         user_input = tk.Entry(window, font=("Serif Sans", 16), )
-        user_input.grid(row=2, column=0)
+        user_input.grid(row=2, column=1)
 
-        answer_button = tk.Button(master=window, text="Answer", command=lambda: answer(character_label, answer_label, user_input, correct_label, incorrect_label))
-        answer_button.grid(row=4, column=0)
+        answer_button = tk.Button(master=window, text="Answer", command=lambda: answer(character_label, answer_label, user_input, correct_label, incorrect_label, ratio_label))
+        answer_button.grid(row=4, column=1)
 
-        next_button = tk.Button(master=window, text="Next Word", command=lambda: next_word(character_label, answer_label, user_input))
-        next_button.grid(row=4, column=1)
+        next_button = tk.Button(master=window, text="Skip Word", command=lambda: next_word(character_label, answer_label, user_input))
+        next_button.grid(row=4, column=3)
 
         next_word(character_label, answer_label, user_input) # runs the next_word function when the program starts
+
+        window.bind('<Return>', lambda command:answer(character_label, answer_label, user_input, correct_label, incorrect_label, ratio_label))
+        window.bind('<Shift_R>', lambda command:next_word(character_label, answer_label, user_input))
 
 
 def next_word(character_label, answer_label, user_input):
@@ -65,28 +72,51 @@ def next_word(character_label, answer_label, user_input):
     rand_word = random.choice(list(CHARACTER_DICT.keys()))
     character_label.config(text=rand_word)
 
-def answer(character_label, answer_label, user_input, correct_label, incorrect_label):
+def answer(character_label, answer_label, user_input, correct_label, incorrect_label, ratio_label):
     """
     Checks to see if the user's input goes with the corresponding 
     """
     word = character_label['text']
+    global CORRECT_POINTS
+    global INCORRECT_POINTS
+    global TOTAL_POINTS
+
     value = CHARACTER_DICT.get(word)
+    correct = CORRECT_POINTS
+    incorrect = INCORRECT_POINTS
+    total = TOTAL_POINTS
+
     if user_input.get() == value:
-        answer_label.config(text="Correct Answer! \nClick the 'Next Word' \nbutton for a new word!")
+        answer_label.config(text="Correct Answer!")
         correct_score = correct_label['text']
-        CORRECT_POINTS = int(''.join(filter(str.isdigit, correct_score)))
-        CORRECT_POINTS +=1
-        correct_label.config(text="Correct: " + str(CORRECT_POINTS))
+        correct = int(''.join(filter(str.isdigit, correct_score)))
+        correct +=1
+        correct_label.config(text="Correct: " + str(correct))
+        total += 1
     else:
-        answer_label.config(text="Incorrect Answer. \nTry again or click the\n'Next Word' button\n for a new word!")
+        answer_label.config(text="Incorrect Answer.")
         incorrect_score = incorrect_label['text']
-        INCORRECT_POINTS = int(''.join(filter(str.isdigit, incorrect_score)))
-        INCORRECT_POINTS += 1
-        incorrect_label.config(text="Incorrect: "+ str(INCORRECT_POINTS))
+        incorrect = int(''.join(filter(str.isdigit, incorrect_score)))
+        incorrect += 1
+        incorrect_label.config(text="Incorrect: "+ str(incorrect))
+        total += 1
+
+    CORRECT_POINTS = correct
+    INCORRECT_POINTS = incorrect
+    TOTAL_POINTS = total
+
+    score_ratio(correct, total, ratio_label)
 
 
-def score_ratio(correct, incorrect):
-    pass
+
+def score_ratio(correct, total, ratio_label):
+    """
+    Creates an accuracy rating
+    """
+    accuracy = (correct / total)*100
+    ratio = (round(accuracy, 2))
+    ratio_string = str(ratio)
+    ratio_label.config(text=ratio_string + "% Accurate")
 
 
 def main():
